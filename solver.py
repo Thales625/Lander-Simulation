59855a19e2183dedeeb1553eb9c64c75fb20f1ff
thales <thaleshend@gmail.com>
2025-08-14 15:58:22 -0300

    add RK4 solver

---
+++ solver.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+class RK4:
+    def __init__(self, state, dSdt) -> None:
+        self.S = np.copy(state)
+        self.dSdt = dSdt
+    
+    def step(self, t, dt):
+        k1 = self.dSdt(self.S, t)
+        k2 = self.dSdt(self.S + 0.5*dt*k1, t + 0.5*dt)
+        k3 = self.dSdt(self.S + 0.5*dt*k2, t + 0.5*dt)
+        k4 = self.dSdt(self.S + dt*k3, t + dt)
+        self.S += (dt/6)*(k1 + 2*k2 + 2*k3 + k4)
+        return self.S
+
+if __name__ == "__main__":
+    def dSdt(S, t):
+        x, y, vx, vy, theta, omega = S
+        return np.array([vx, vy, 0, -1.62, omega, 0.0])
+
+    S0 = np.array([
+        0., # position
+        0.,
+
+        0., # velocity
+        0.,
+
+        0.0, # angle
+
+        0.1, # angular velocity
+    ])
+
+    solver = RK4(S0, dSdt)
+
+    for i in range(10): print(solver.step(0, 0.1))
